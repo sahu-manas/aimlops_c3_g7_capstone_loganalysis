2407966417335fc970d733ec38e1c96cd7806d07
msahu <msahu@opentext.com>
2024-12-12 14:49:55 +0530

    added files for HDFS

---
+++ hdfs_analysis/init.sh
@@ -0,0 +1,16 @@
+file="./output/"
+if [ -e $file ]
+then
+  echo "$file exists"
+else
+  mkdir -p $file
+fi
+
+file="./output/bert"
+if [ -e $file ]
+then
+  echo "$file exists"
+else
+  mkdir -p $file
+fi
+
